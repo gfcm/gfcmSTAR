fbcd4dbbefe809a9b6390d5eb0aada72a2c9d1b9
Arni Magnusson <thisisarni@gmail.com>
2021-04-19 16:31:12 +0200

    Empty package

---
+++ R/gfcmSTAR-package.R
@@ -0,0 +1,13 @@
+#' @docType package
+#'
+#' @name gfcmSTAR-package
+#'
+#' @aliases gfcmSTAR
+#'
+#' @title Tools to Work with GFCM STAR
+#'
+#' @description
+#' Tools that support the Stock Assessment Results (STAR) framework of the
+#' General Fisheries Commission for the Mediterranean.
+
+NA
