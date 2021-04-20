0d2dfc882601b4a2540aadd361289aae4116bb5d
Arni Magnusson <thisisarni@gmail.com>
2021-04-20 10:52:28 +0200

    Internal object gsa.table

---
+++ R/gsa.table.R
@@ -0,0 +1,15 @@
+gsa.table <- data.frame(
+Code=c("1","2","3","4","5","6","7","8","9","10","11.1","11.2","12","13","14",
+       "15","16","17","18","19","20","21","22","23","24","25","26","27","28",
+       "29","30"),
+Name=c("Northern Alboran Sea","Alboran Island","Southern Alboran Sea","Algeria",
+       "Balearic Islands","Northern Spain","Gulf of Lion","Corsica",
+       "Ligurian Sea and Northern Tyrrhenian Sea",
+       "Southern and Central Tyrrhenian Sea","Western Sardinia",
+       "Eastern Sardinia","Northern Tunisia","Gulf of Hammamet","Gulf of Gabes",
+       "Malta","Southern Sicily","Northern Adriatic Sea",
+       "Southern Adriatic Sea","Western Ionian Sea","Eastern Ionian Sea",
+       "Southern Ionian Sea","Aegean Sea","Crete","Northern Levant Sea",
+       "Cyprus","Southern Levant Sea","Eastern Levant Sea","Marmara Sea",
+       "Black Sea","Azov Sea"),
+stringsAsFactors=FALSE)
