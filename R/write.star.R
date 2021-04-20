5aa3d0b2ce9ae20937ecb0271b6143205b6b7055
Arni Magnusson <thisisarni@gmail.com>
2021-04-21 01:02:21 +0200

    Help page for write.star()

--- R/write.star.R
+++ R/write.star.R
@@ -1,4 +1,12 @@
-#' @rdname gfcmSTAR-internal
+#' Write STAR Object to Files
+#'
+#' Write a STAR object to files.
+#'
+#' @param x a STAR object, list containing \code{Metadata} and
+#'        \code{TimeSeries}.
+#' @param mfile a filename for the metadata.
+#' @param tfile a filename for the time series.
+#' @param \dots passed to \code{write.csv}.
 #'
 #' @importFrom utils write.csv
 #'
@@ -6,10 +14,10 @@
 
 ## Write
 
-write.star <- function(star, mfile="metadata.csv", tfile="timeseries.csv", ...)
+write.star <- function(x, mfile="metadata.csv", tfile="timeseries.csv", ...)
 {
-  metadata <- as.data.frame(star$Metadata, stringsAsFactors=FALSE)
-  timeseries <- star$TimeSeries
+  metadata <- as.data.frame(x$Metadata, stringsAsFactors=FALSE)
+  timeseries <- x$TimeSeries
   write.csv(metadata, file=mfile, ...)
   write.csv(timeseries, file=tfile, ...)
 }
