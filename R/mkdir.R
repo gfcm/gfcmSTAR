6ae18cf05ad28b3f11428672797180c96895dcb8
Arni Magnusson <thisisarni@gmail.com>
2021-04-21 02:04:38 +0200

    Internal function mkdir()

---
+++ R/mkdir.R
@@ -0,0 +1,12 @@
+#' @rdname gfcmSTAR-internal
+#'
+#' @export
+
+## Create directory, including parent dirs if necessary, without generating a
+## warning if dir already exists.
+
+mkdir <- function(path)
+{
+  out <- sapply(path, dir.create, showWarnings=FALSE, recursive=TRUE)
+  invisible(out)
+}
